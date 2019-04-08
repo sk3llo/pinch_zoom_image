1e31bad16c94f4f6ee7b6533ce57fa24228bebcf
Anton Karpenko <kapraton@gmail.com>
2019-04-08 02:53:25 -0400

    Update SwiftPinchZoomImagePlugin.swift

--- ios/Classes/SwiftPinchZoomImagePlugin.swift
+++ ios/Classes/SwiftPinchZoomImagePlugin.swift
@@ -10,10 +10,12 @@ public class SwiftPinchZoomImagePlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if (call.method == "hideStatusBar") {
-        UIApplication.shared.keyWindow?.windowLevel = UIWindow.Level.statusBar
+      UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelStatusBar
+//        UIApplication.shared.keyWindow?.windowLevel = UIWindow.Level.statusBar
         result(nil)
     } else if (call.method == "showStatusBar") {
-        UIApplication.shared.keyWindow?.windowLevel = UIWindow.Level.normal
+//      UIApplication.shared.keyWindow?.windowLevel = UIWindow.Level.normal
+      UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelNormal
         result(nil)
     }
   }
