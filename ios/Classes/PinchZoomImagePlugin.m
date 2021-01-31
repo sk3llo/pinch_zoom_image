c5c0b53df490c51d28bb2f299afce862417a340f
Anton Karpenko <kapraton@gmail.com>
2021-01-31 11:12:41 -0500

    Update PinchZoomImagePlugin.m

--- ios/Classes/PinchZoomImagePlugin.m
+++ ios/Classes/PinchZoomImagePlugin.m
@@ -1,12 +1,5 @@
 #import "PinchZoomImagePlugin.h"
-#if __has_include(<pinch_zoom_image_last/pinch_zoom_image_last-Swift.h>)
-#import <pinch_zoom_image_last/pinch_zoom_image_last-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "pinch_zoom_image_last-Swift.h"
-#endif
+#import <pinch_zoom_image/pinch_zoom_image-Swift.h>
 
 @implementation PinchZoomImagePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
